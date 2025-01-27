07dfed08ce38df5fa28a9d344e60f22555bfe870
Priyanshu Tiwari <priyanshu16181389@gmail.com>
2025-01-28 00:14:46 +0530

    added metamask

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 import { http, createConfig } from "wagmi";
 import {  sepolia } from "wagmi/chains";
-import {  injected } from "wagmi/connectors";
+import {  injected, metaMask } from "wagmi/connectors";
 
 
 
 export const config = createConfig({
     chains: [ sepolia],
-    connectors: [injected()],
+    connectors: [injected(), metaMask()],
     transports: {
         [sepolia.id]: http()
     }
